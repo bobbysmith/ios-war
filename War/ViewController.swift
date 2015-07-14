2065a61fadc8581b956f6e9e492d495ff37e443f
Bobby Smith <bobbysmith24@gmail.com>
2015-07-13 21:08:03 -0500

    added image assets

--- War/ViewController.swift
+++ War/ViewController.swift
@@ -21,7 +21,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-//        self.playButton.setTitle("Go", forState: UIControlState.Normal)
     }
 
     override func didReceiveMemoryWarning() {
